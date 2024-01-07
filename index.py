e64c062c9008facd6ffb0c32b0027a562adaebdb
camillie15 <138990540+camillie15@users.noreply.github.com>
2024-01-07 01:08:19 -0500

    update index.py

--- index.py
+++ index.py
@@ -7,9 +7,5 @@ app = Flask(__name__, static_folder='static')
 def home():
     return render_template('index.html')
 
-@app.route('/createNewEvent')
-def create_New_Event():
-    return render_template('layout2.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
