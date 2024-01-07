49f5796c179f40a40d1ff61226e4aac426b98071
camillie15 <138990540+camillie15@users.noreply.github.com>
2024-01-06 23:04:06 -0500

    update index.py

--- index.py
+++ index.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def home():
-    return render_template('layout.html')
+    return render_template('index.html')
+
+@app.route('/createNewEvent')
+def create_New_Event():
+    return render_template('layout2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
